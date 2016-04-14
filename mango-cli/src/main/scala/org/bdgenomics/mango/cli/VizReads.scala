0247dfd1b14f5208a76b0580f93af37bc0cf601c
Eric Tu <erictctu@gmail.com>
2016-04-14 16:32:35 -0700

    using dataFrames for variant and variantfreq

--- mango-cli/src/main/scala/org/bdgenomics/mango/cli/VizReads.scala
+++ mango-cli/src/main/scala/org/bdgenomics/mango/cli/VizReads.scala
@@ -25,6 +25,7 @@ import net.liftweb.json.Serialization.write
 import org.apache.parquet.filter2.dsl.Dsl._
 import org.apache.parquet.filter2.predicate.FilterPredicate
 import org.apache.spark.rdd.RDD
+import org.apache.spark.storage.StorageLevel
 import org.apache.spark.{ Logging, SparkContext }
 import org.bdgenomics.adam.models.{ ReferenceRegion, SequenceDictionary }
 import org.bdgenomics.adam.projections.{ FeatureField, Projection }
@@ -38,6 +39,7 @@ import org.bdgenomics.mango.models.GenotypeMaterialization
 import org.bdgenomics.mango.tiling.AlignmentRecordTile
 import org.bdgenomics.utils.cli._
 import org.bdgenomics.utils.instrumentation.Metrics
+import org.apache.spark.sql.{ DataFrame, SQLContext }
 import org.fusesource.scalate.TemplateEngine
 import org.kohsuke.args4j.{ Argument, Option => Args4jOption }
 import org.scalatra.ScalatraServlet
@@ -71,6 +73,7 @@ object VizReads extends BDGCommandCompanion with Logging {
   val commandDescription: String = "Genomic visualization for ADAM"
 
   var sc: SparkContext = null
+  var sqlContext: SQLContext = null
   var faWithIndex: Option[IndexedFastaSequenceFile] = None
   var partitionCount: Int = 0
   var referencePath: String = ""
@@ -84,7 +87,7 @@ object VizReads extends BDGCommandCompanion with Logging {
   var globalDict: SequenceDictionary = null
   var refRDD: ReferenceRDD = null
   var readsData: AlignmentRecordTile = null
-  var variantData: GenotypeMaterialization = null
+  var varData: VariantLayout = null
   var server: org.eclipse.jetty.server.Server = null
   var screenSize: Int = 1
 
@@ -295,16 +298,7 @@ class VizServlet extends ScalatraServlet {
       contentType = "json"
       val viewRegion = ReferenceRegion(params("ref"), params("start").toLong,
         VizUtils.getEnd(params("end").toLong, VizReads.globalDict(params("ref").toString)))
-      val variantRDDOption = VizReads.variantData.multiget(viewRegion, VizReads.variantsPaths)
-      variantRDDOption match {
-        case Some(_) => {
-          val variantRDD: RDD[(ReferenceRegion, Genotype)] = variantRDDOption.get.toRDD()
-          write(VariantLayout(variantRDD))
-        } case None => {
-          write("")
-        }
-      }
-
+      write(VizReads.varData.get(viewRegion))
     }
   }
 
@@ -313,15 +307,8 @@ class VizServlet extends ScalatraServlet {
       contentType = "json"
       val viewRegion = ReferenceRegion(params("ref"), params("start").toLong,
         VizUtils.getEnd(params("end").toLong, VizReads.globalDict(params("ref").toString)))
-      val variantRDDOption = VizReads.variantData.multiget(viewRegion, VizReads.variantsPaths)
-      variantRDDOption match {
-        case Some(_) => {
-          val variantRDD: RDD[(ReferenceRegion, Genotype)] = variantRDDOption.get.toRDD()
-          write(VariantFreqLayout(variantRDD))
-        } case None => {
-          write("")
-        }
-      }
+      val results = VizReads.varData.getFreq(viewRegion)
+      write(results)
     }
   }
 
@@ -383,6 +370,7 @@ class VizReads(protected val args: VizReadsArgs) extends BDGSparkCommand[VizRead
 
   override def run(sc: SparkContext): Unit = {
     VizReads.sc = sc
+    VizReads.sqlContext = new SQLContext(sc)
 
     VizReads.partitionCount =
       if (args.partitionCount <= 0)
@@ -434,17 +422,15 @@ class VizReads(protected val args: VizReadsArgs) extends BDGSparkCommand[VizRead
         }
       case None => log.info("No preprocessing file provided")
     }
-    VizReads.variantData = GenotypeMaterialization(sc, VizReads.globalDict, VizReads.partitionCount)
     val variantsPath = Option(args.variantsPaths)
     variantsPath match {
       case Some(_) => {
         VizReads.variantsPaths = args.variantsPaths.split(",").toList
         VizReads.variantsExist = true
         for (varPath <- VizReads.variantsPaths) {
-          if (varPath.endsWith(".vcf")) {
-            VizReads.variantData.loadSample(varPath)
-          } else if (varPath.endsWith(".adam")) {
-            VizReads.variantData.loadSample(varPath)
+          if (varPath.endsWith(".adam")) {
+            VizReads.varData = new VariantLayout(VizReads.sc)
+            VizReads.varData.loadChr(varPath)
           } else {
             log.info("WARNING: Invalid input for variants file")
             println("WARNING: Invalid input for variants file")
