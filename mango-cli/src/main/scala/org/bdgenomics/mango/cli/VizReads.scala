fdc82f820f67c46bed7b95c31df5b612497ab7cb
Alyssa Morrow <akmorrow@berkeley.edu>
2016-04-15 10:33:55 -0700

    completed frequencyRDD with data frames

--- mango-cli/src/main/scala/org/bdgenomics/mango/cli/VizReads.scala
+++ mango-cli/src/main/scala/org/bdgenomics/mango/cli/VizReads.scala
@@ -111,6 +111,7 @@ object VizReads extends BDGCommandCompanion with Logging {
 
   //Correctly shuts down the server
   def quit() {
+    // TODO: save frequencies
     val thread = new Thread {
       override def run() {
         try {
